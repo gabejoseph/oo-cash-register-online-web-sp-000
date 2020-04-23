e2354cf2fa23fd884e0a8f2ac91e006fc1ed1c3b
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-23 20:34:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 0)
-    until quantity = 0
+    until quantity == 0
       @total += price*quantity
     else 
       @total += price 
