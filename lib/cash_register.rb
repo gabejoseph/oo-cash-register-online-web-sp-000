45db86e8870b5241774ba69fae861faac3a628f4
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-23 21:14:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,13 +32,14 @@ class CashRegister
   
   def void_last_transaction
     #only going to check price, reverse of add_item
-    if @items.size > 0 
-      @items.pop
-      @total = @total-@price
-    else 
-      @total = 0
-    end
-    @total
+    
+  #   if @items.size > 0 
+  #     @items.pop
+  #     @total = @total-@price
+  #   else 
+  #     @total = 0
+  #   end
+  #   @total
   end
 
 end
